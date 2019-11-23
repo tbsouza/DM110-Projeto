2c8014a712a9c5130eb501876509823bb8bb78e1
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-11-23 09:13:14 -0300

    Create the OrderBean base class

--- Projeto/ws-dm110/2019-dm110/dm110-ejb/src/main/java/br/inatel/dm110/order/beans/OrderBean.java
+++ Projeto/ws-dm110/2019-dm110/dm110-ejb/src/main/java/br/inatel/dm110/order/beans/OrderBean.java
@@ -1,5 +1,71 @@
 package br.inatel.dm110.order.beans;
 
-public class OrderBean {
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import br.inatel.dm110.api.OrderTO;
+import br.inatel.dm110.order.entities.Order;
+import br.inatel.dm110.order.interfaces.OrderLocal;
+import br.inatel.dm110.order.interfaces.OrderRemote;
+
+@Stateless
+@Remote(OrderRemote.class)
+@Local(OrderLocal.class)
+public class OrderBean implements OrderLocal, OrderRemote {
+
+	@Override
+	public void saveOrder(OrderTO orderTO) {
+
+		Order order = mapToOrder(orderTO);
+
+		System.out.println("Saving new Order: " + order.getOrderCode());
+
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public OrderTO getOrderById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderTO> getAllOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateOrderById(OrderTO orderTO) {
+
+		Order order = mapToOrder(orderTO);
+
+		System.out.println("Updating Order: " + order.getOrderCode());
+
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Map OrderTO to Order
+	 *
+	 * @param orderTO
+	 * @return The Order mapped
+	 */
+	private Order mapToOrder(OrderTO orderTO) {
+
+		Order order = new Order();
+
+		order.setAmount(orderTO.getAmount());
+		order.setCpf(orderTO.getCpf());
+		order.setOrderCode(orderTO.getOrderCode());
+		order.setProducCode(orderTO.getProducCode());
+		order.setSaleDate(orderTO.getSaleDate());
+		order.setValue(orderTO.getValue());
+
+		return order;
+	}
 
 }
