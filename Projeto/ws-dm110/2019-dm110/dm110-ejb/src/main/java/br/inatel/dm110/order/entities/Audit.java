8337ff9f6ead8af8e929687f36b332c00c04fc4e
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-11-23 15:12:47 -0300

    Create Audit and AuditTO entities

---
+++ Projeto/ws-dm110/2019-dm110/dm110-ejb/src/main/java/br/inatel/dm110/order/entities/Audit.java
@@ -0,0 +1,43 @@
+package br.inatel.dm110.order.entities;
+
+import java.util.Date;
+
+public class Audit {
+
+	private int id; // auto increment
+	private int registerCode; // Order code
+	private String operation; // Create, update, list
+	private Date dateTime;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRegisterCode() {
+		return registerCode;
+	}
+
+	public void setRegisterCode(int registerCode) {
+		this.registerCode = registerCode;
+	}
+
+	public String getOperation() {
+		return operation;
+	}
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+
+	public Date getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
+}
