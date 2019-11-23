ef26d9eac1fa2f829df7908a0792bee578b64349
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-11-23 09:24:35 -0300

    Create base OrderDao class
    - Create base OrderDao class to performe database operations
      (List All Orders, List Order by Id, Update Order, Inser new Order)

--- Projeto/ws-dm110/2019-dm110/dm110-ejb/src/main/java/br/inatel/dm110/order/dao/OrderDao.java
+++ Projeto/ws-dm110/2019-dm110/dm110-ejb/src/main/java/br/inatel/dm110/order/dao/OrderDao.java
@@ -1,5 +1,37 @@
 package br.inatel.dm110.order.dao;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import br.inatel.dm110.order.entities.Order;
+
+@Stateless
 public class OrderDao {
+
+	@PersistenceContext(unitName = "order_pu")
+	private EntityManager em;
+
+	public List<Order> listAll() {
+		Query query = em.createQuery("from Order o", Order.class);
+		return query.getResultList();
+	}
+
+	public void insert(Order order) {
+		em.persist(order);
+	}
+
+	public Order getOrder(Order order) {
+		// TODO Do the correct query
+		Query query = em.createQuery("from Order o where ", Order.class);
+		return (Order) query.getSingleResult();
+	}
+
+	public void updateOrder(Order order) {
+		// TODO
+	}
 
 }
