53f7d8593c5eadb003bed57ab1a28f3d868a0c8a
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-11-24 10:11:37 -0300

    Add OrderServiceImpl to the list of RestApplication

--- Projeto/ws-dm110/2019-dm110/dm110-web/src/main/java/br/inatel/dm110/rest/RestApplication.java
+++ Projeto/ws-dm110/2019-dm110/dm110-web/src/main/java/br/inatel/dm110/rest/RestApplication.java
@@ -9,6 +9,7 @@ import javax.ws.rs.core.Application;
 import br.inatel.dm110.impl.CalculatorServiceImpl;
 import br.inatel.dm110.impl.HelloServiceImpl;
 import br.inatel.dm110.impl.IbgeServiceImpl;
+import br.inatel.dm110.impl.OrderServiceImpl;
 
 @ApplicationPath("/api")
 public class RestApplication extends Application {
@@ -16,10 +17,8 @@ public class RestApplication extends Application {
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> classes = new HashSet<>();
-		
-		classes.add(HelloServiceImpl.class);
-		classes.add(CalculatorServiceImpl.class);
-		classes.add(IbgeServiceImpl.class);
+
+		classes.add(OrderServiceImpl.class);
 		
 		return classes;
 	}
