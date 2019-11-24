968e21dfad29ff345dfb88115671aabd04ea75ab
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-11-24 10:21:29 -0300

    Add serialVersionUID on Audit entity

--- Projeto/ws-dm110/2019-dm110/dm110-ejb/src/main/java/br/inatel/dm110/order/entities/Audit.java
+++ Projeto/ws-dm110/2019-dm110/dm110-ejb/src/main/java/br/inatel/dm110/order/entities/Audit.java
@@ -13,6 +13,8 @@ import javax.persistence.Table;
 @Table(name = "AUDIT") // Table name in database
 public class Audit implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id; // auto increment
