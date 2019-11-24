06adeda8e49a0b0e2a779a765af88883f4abe35c
Thiago Barbosa de Souza <tbsouza@outlook.com>
2019-11-24 10:27:41 -0300

    Remove unecessary imports in RestApplication

--- Projeto/ws-dm110/2019-dm110/dm110-web/src/main/java/br/inatel/dm110/rest/RestApplication.java
+++ Projeto/ws-dm110/2019-dm110/dm110-web/src/main/java/br/inatel/dm110/rest/RestApplication.java
@@ -6,9 +6,6 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import br.inatel.dm110.impl.CalculatorServiceImpl;
-import br.inatel.dm110.impl.HelloServiceImpl;
-import br.inatel.dm110.impl.IbgeServiceImpl;
 import br.inatel.dm110.impl.OrderServiceImpl;
 
 @ApplicationPath("/api")
